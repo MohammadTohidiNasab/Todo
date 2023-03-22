df64a60a147c901524261e7c73ce384329573ba5
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-03-22 08:33:46 -0700

    add static files

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('todo.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+
