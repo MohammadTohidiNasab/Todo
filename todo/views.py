b3c88df3d914ec3e06a4fbb3b82db2c56f73e79f
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-03-22 11:16:15 -0700

    ,

--- todo/views.py
+++ todo/views.py
@@ -39,17 +39,12 @@ def update(request, todo_id):
     todo.save()
     return redirect('todos:index')
 
+
 #api
-
-
 
 class ListTodo(generics.ListAPIView):
     queryset = Todo.objects.all()
     serializer_class = ToDoSerializer
-
-# # class CreateTodo(generics.CreateAPIView):
-# #     queryset = Todo.objects.all()
-# #     serializer_class = ToDoSerializer
 
 
 class Cred (generics.RetrieveUpdateDestroyAPIView):
