a62c8841f81d83ab2c8295ea14d5b248d41bc235
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-10-06 21:22:57 +0330

    add gitignore

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,7 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('todo.urls')),
-]
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
 
