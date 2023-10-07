c6b38972e2d2b83c0f8e292ded486df6e140acb7
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-10-07 22:36:35 +0330

    change

--- config/settings.py
+++ config/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
     
-    
+    #schema setting for api
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     
     
