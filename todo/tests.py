ef080e02e1d3595f8c522fa93e02773bbae1edca
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-10-09 14:49:10 +0330

    test todo model

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import Todo
+from model_bakery import baker 
+'''baker fill out inputs you dont need to test'''
 # Create your tests here.
+
+
+class TestTodoModel(TestCase):
+    def test_model_str(self):
+        new_todo = baker.make(Todo, title = 'todo')
+        self.assertEqual(str(new_todo), 'todo')
